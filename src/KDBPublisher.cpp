ec187166e444bca8d2aa7f585bbd64cc52ed21e1
Ferenc Bodon <fbodon@kx.com>
2020-03-21 17:09:51 +0100

    more verbose error handling

--- src/KDBPublisher.cpp
+++ src/KDBPublisher.cpp
@@ -9,7 +9,8 @@ KDBPublisher::KDBPublisher(unsigned long triggerNr, const char* argv[]) {
     socket = khpu(S(argv[0]), atoi(argv[1]), S("Administrator:password")); 
     if (socket < 0) {
         /* problem connecting */
-        cerr << "Problem connecting to kdb. Error code " << socket << endl;
+        cerr << "Problem connecting to kdb at " << argv[0] << ":" << argv[1] 
+             << ". Error code " << socket << endl;
         exit(1);
     }
     cout << "Connection to " << argv[0] << ":" << argv[1] << " was successful" << endl;
