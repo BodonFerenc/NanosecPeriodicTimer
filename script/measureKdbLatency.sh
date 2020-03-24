6458546d9d8231705f1b2509c0679f1db5cce256
Ferenc Bodon <fbodon@kx.com>
2020-03-24 12:27:50 +0100

    using getopt

--- script/measureKdbLatency.sh
+++ script/measureKdbLatency.sh
@@ -2,65 +2,98 @@
 
 set -eu -o pipefail
 
-if test "$#" -lt 4; then
-   echo "Please provide four paramters:
-      - output csv file name of the publisher, e.g. out/publisher.csv 
-      - frequency, e.g. 5000, 
-      - test duration in seconds e.g. 120, 
-      - an output file name to store statistics, e.g. stat.csv"
-   exit 1
-fi
+VERBOSE=1
+FREQ=30000
+DURATION=10
+PUBLISHEROUTPUT=/tmp/timer.csv
+OUTPUT=stat.csv
+KDBHOST=0.0.0.0
+RDBOUTPUTFILENAME=/tmp/rdb.csv
 
-PUBLISHEROUTPUT=$1
-FREQ=$2
-DURATION=$3
-OUTPUT=$4
+function args()
+{
+    options=$(getopt -o q --long tcp --long freq: --long dur: --long output: -- "$@")
+    [ $? -eq 0 ] || {
+        echo "Incorrect option provided"
+        exit 1
+    }
+    eval set -- "$options"
+    while true; do
+        case "$1" in
+        -q)
+            VERBOSE=0
+            ;;
+        --tcp)
+            KDBHOST=localhost
+            ;;            
+        --freq)
+            shift; # The arg is next in position args
+            FREQ=$1
+            ;;
+        --dur)
+            shift; # The arg is next in position args
+            DURATION=$1
+            ;;
+        --output)
+            shift; # The arg is next in position args
+            OUTPUT=$1
+           ;;
+        --)
+            shift
+            break
+            ;;
+        esac
+        shift
+    done
+}
+
+function log () {
+    if [[ $VERBOSE -eq 1 ]]; then
+        echo "$@"
+    fi
+}
+
+args $0 "$@"
+
 
 LOGDIR=../log
 mkdir -p $LOGDIR
 
-rdbOutputFileName=/tmp/rdb.csv
-echo "Starting RDB script in the background..."
-nohup numactl --physcpubind=0 q rdb_latency.q -output $rdbOutputFileName -p 5001 2>&1 > ${LOGDIR}/rdb.txt &
+
+log "Starting RDB script in the background..."
+nohup numactl --physcpubind=0 q rdb_latency.q -output $RDBOUTPUTFILENAME -p 5001 2>&1 > ${LOGDIR}/rdb.txt 2>&1 &
 RDB_PID=$!
 
-nohup perf stat -x " " -p $RDB_PID -e task-clock --log-fd 1 > ${LOGDIR}/perf.txt &
+nohup perf stat -x " " -p $RDB_PID -e task-clock --log-fd 1 > ${LOGDIR}/perf.txt 2>&1 &
 PERF_PID=$!
 
 sleep 1
 
-echo
+log
 
-echo "Starting publisher with frequency $FREQ for duration $DURATION"
-if [ -z ${USETCP+x} ]; 
-then 
-  host=0.0.0.0
-  echo "using unix socket"
-else
-  host=localhost
-  echo "using TCP/IP"
-fi
-numactl --physcpubind=1 ../bin/KDBPublishLatencyTester $FREQ $DURATION $PUBLISHEROUTPUT $host 5001
+log "Starting publisher with frequency $FREQ for duration $DURATION"
+numactl --physcpubind=1 ../bin/KDBPublishLatencyTester $FREQ $DURATION $PUBLISHEROUTPUT $KDBHOST 5001 > ${LOGDIR}/publisher.txt
 
 tempOutputFileName=/tmp/stat.csv
-echo
-echo "Processing output $PUBLISHEROUTPUT"
+log
+log "Processing output $PUBLISHEROUTPUT"
 q generatePublisherLatencyStats.q $PUBLISHEROUTPUT $tempOutputFileName -q
 
-echo "Waiting for perf stat (PID: $PERF_PID) and RDB (PID: $RDB_PID) to finish"
-wait $RDB_PID
+log "Waiting for perf stat (PID: $PERF_PID) and RDB (PID: $RDB_PID) to finish"
 wait $PERF_PID
 
 RDBCPUUSAGE=($(cat ${LOGDIR}/perf.txt))
-echo "RDB CPU usage ${RDBCPUUSAGE[5]}"
+log "RDB CPU usage ${RDBCPUUSAGE[5]}"
 
 METAFILE=/tmp/rdbperfmeta.csv
 
-echo "Creating csv for the meta data..."
+log "Creating csv for the meta data..."
 echo "frequency,duration,RDB CPU Usage" > $METAFILE
 echo "$FREQ,$DURATION,${RDBCPUUSAGE[5]}" >> $METAFILE
 
-echo "Merging meta data with publisher and RDB statistics"
-paste -d, $METAFILE $tempOutputFileName $rdbOutputFileName > $OUTPUT
+log "Merging meta data with publisher and RDB statistics into $OUTPUT"
+paste -d, $METAFILE $tempOutputFileName $RDBOUTPUTFILENAME > $OUTPUT
+
+rm $METAFILE
 
 exit 0
