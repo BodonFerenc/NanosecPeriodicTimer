75a036e5fbbff680332b5171befc9304a9b6c4d3
Ferenc Bodon <fbodon@kx.com>
2020-03-26 10:29:27 +0100

    using symbol generator

--- src/KDBPublisherCSVLoggerTask.hpp
+++ src/KDBPublisherCSVLoggerTask.hpp
@@ -19,14 +19,18 @@
 #include <array>
 #include "CSVLoggerTask.hpp"
 #include "KDBPublisher.hpp"
-
-constexpr auto STOCKNR = 26 * 26 * 26 * 26;
+#include "KDBSymGenerator.hpp"
 
 template<bool FLUSH>
 class KDBPublisherCSVLoggerTask: public CSVLoggerTask, public KDBPublisher {
     protected:  
-        std::vector<std::array<char, 5>> stockUniverse;
-        std::vector<char*> stockToSend;        
+        KDBSymGenerator symGenerator;
+        
+        // fields to send. We dont really care about the content.
+        char stop = 'i';            /* charactor example */
+        int size = 444;             /* long example */
+        double price = 12.12;       /* double example */
+        char condition = 'c';      
 
     public: 
         KDBPublisherCSVLoggerTask(unsigned long, const char* argv[]);
@@ -35,49 +39,19 @@ class KDBPublisherCSVLoggerTask: public CSVLoggerTask, public KDBPublisher {
 
 template<bool FLUSH>
 KDBPublisherCSVLoggerTask<FLUSH>::KDBPublisherCSVLoggerTask(unsigned long triggerNr, const char* argv[]) 
-    : CSVLoggerTask(triggerNr, argv), KDBPublisher(triggerNr, argv+1) {  
-    tableName = (ks((S) "trade"));
-
-    std::srand(std::time(nullptr));
-
-    stockUniverse.resize(STOCKNR);
-    unsigned int i=0;
-    for (char a = 'A'; a <= 'Z'; ++a) {
-        for (char b = 'A'; b <= 'Z'; ++b) {
-            for (char c = 'A'; c <= 'Z'; ++c) {
-                for (char d = 'A'; d <= 'Z'; ++d) {
-                    stockUniverse[i][0] = a;
-                    stockUniverse[i][1] = b;
-                    stockUniverse[i][2] = c;
-                    stockUniverse[i][3] = d;
-                    stockUniverse[i][4] = '\0';
-                    ++i;
-                }
-            }
-        }
-    }
-
-    stockToSend.reserve(triggerNr);
-    for (i = 0; i< triggerNr; ++i) {
-        stockToSend.push_back(stockUniverse[rand() % STOCKNR].data());
-    }
-
+    : CSVLoggerTask(triggerNr, argv), KDBPublisher(triggerNr, argv+1), symGenerator{triggerNr} {  
+    tableName = (ks((S) "trade"));    
 }
 
 template<bool FLUSH>
 bool inline KDBPublisherCSVLoggerTask<FLUSH>::run(const TIME& expected, const TIME& real) {
-    static char stop = 'i';            /* charactor example */
-    static int size = 444;            /* long example */
-    static double price = 12.12;       /* double example */
-    static char condition = 'c'; 
-
     unsigned long sq = expectedTriggerTimes.size();
-    char* const stock = stockToSend[sq];
 
-    K row = knk(7, ks(stock), kj(sq), kc(stop), ki(size), kf(price), kc(condition), 
+    K row = knk(7, ks(*symGenerator.sym_it), kj(sq), kc(stop), ki(size), kf(price), kc(condition), 
         ktj(-KP, DURNANO((std::chrono::system_clock::now() - kdb_start).time_since_epoch())));
 
     bool res = KDBPublisher::sendUpdate<FLUSH>(row);
+    ++symGenerator.sym_it;
 
     CSVLoggerTask::run(expected, real);
     return res;
