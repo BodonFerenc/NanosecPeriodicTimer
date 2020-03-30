86635ac61d0536ac3b71fa1e20f9ec4d3b4b332a
Ferenc Bodon <fbodon@kx.com>
2020-03-30 19:49:08 +0200

    fixing latency runner script impacting e.g. RDB CPU usage

--- script/startPublisherAndRDB.sh
+++ script/startPublisherAndRDB.sh
@@ -75,13 +75,15 @@ sleep 1   # wait a sec till RDB comes up. TODO: implement more robust solution
 log
 
 log "Starting publisher with frequency $FREQ for duration $DURATION"
-numactl --physcpubind=1 ../bin/KDBPublishLatencyTester $FREQ $DURATION $PUBLISHEROUTPUT $KDBHOST 5001 $FLUSH $BATCHSIZE $BATCHTYPE> ${LOGDIR}/publisher.txt
+nohup numactl --physcpubind=1 ../bin/KDBPublishLatencyTester $FREQ $DURATION $PUBLISHEROUTPUT $KDBHOST 5001 $FLUSH $BATCHSIZE $BATCHTYPE> ${LOGDIR}/publisher.txt  2>&1 &
+PUB_PID=$!
 
 afterStartWork
 
 nohup perf stat -x " " -p $RDB_PID -e task-clock --log-fd 1 > ${LOGDIR}/perf.txt 2>&1 &
 PERF_PID=$!
 
+wait $PUB_PID
 TIMERSTATFILE=/tmp/stat.csv
 log
 log "Processing output $PUBLISHEROUTPUT"
